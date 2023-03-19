e8e9db7a1ee151aa45165a890abb74a1503f8b9f
Galang Setiawan <52562711+GalangSetiawan@users.noreply.github.com>
2023-03-19 09:29:24 +0700

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,25 +1,9 @@
 // for localhost
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "sejarah-joang",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
-
-
-//hosting 
 // module.exports = {
 //   HOST: "localhost",
-//   USER: "desawi20",
-//   PASSWORD: "@27GalangCpanel",
-//   DB: "desawi20_wisatacisaat",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "sejarah-joang",
 //   dialect: "mysql",
 //   pool: {
 //     max: 5,
@@ -28,3 +12,19 @@ module.exports = {
 //     idle: 10000
 //   }
 // };
+
+
+// for hosting 
+module.exports = {
+  HOST: "localhost",
+  USER: "desawi20",
+  PASSWORD: "@27GalangCpanel",
+  DB: "desawi20_sejarahjoang",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
